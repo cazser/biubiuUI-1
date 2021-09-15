e108687435e6868b5fe99d825d6e79e36881cb81
cazser <2264712072@qq.com>
2021-09-15 09:57:31 +0800

    不知道vue3怎么测试

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import { createApp } from 'vue'
+import { createApp, defineComponent } from 'vue'
 
 import App from '@/app.vue'
 import Icon from '@/icon.vue'
 import ButtonGroup from '@/button-group.vue'
-
+import Button from '@/button.vue'
 const app = createApp(App)
 app.component('biu-icon', Icon)
 app.component('biu-button-group', ButtonGroup)
 app.mount('#app')
+
+{
+
+    const button = defineComponent(Button, {
+        propsData: {
+            icon: "left"
+        }
+    })
+
+    console.log(button.$el)
+
+}
