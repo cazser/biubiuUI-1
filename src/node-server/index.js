4c938eaf166a109b609787bdd11d90cc7d21c63c
cazser <2264712072@qq.com>
2021-09-18 20:33:40 +0800

    使用ajax

--- src/node-server/index.js
+++ src/node-server/index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const multer = require('multer')
-const upload = multer({ dest: "yyy/" })
+const cors = require('cors')
+const upload = multer({ dest: "uploads/" })
 const app = express()
-app.get('/', (request, response) => {
-    response.send('hello  nodejs ')
+app.options('/upload', cors())
+app.get('/', cors(), (request, response) => {
+    //response.send('hello  nodejs ')
 })
-app.post('/upload', upload.single('xxx'), (request, response) => {
-    console.log(request.file)
-    response.send(request.file.filename)
+app.post('/upload', cors(), upload.single('file'), (request, response) => {
+    try {
+        response.set({ 'Access-Control-Allow-Origin': '*' })
+        response.send(request.file.filename)
+    } catch (e) {
+        console.log(e)
+    }
 })
 
+app.get('/preview/:key', cors(), (request, response) => {
+    console.log(request.params.key)
+    response.sendFile(`uploads/${request.params.key}`, {
+        root: __dirname,
+        headers: {
+            "Content-Type": 'img/jpg'
+        }
+    }, (error) => {
+        console.log(error);
+    })
+})
 app.listen(3000)
